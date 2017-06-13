70eae00be478195dc39d854db57197264a9ca71a
jfonz412 <jfonzone@outlook.com>
2017-06-13 14:39:51 -0400

    events controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 	get    '/login',    to: 'sessions#new'
 	post   '/login',    to: 'sessions#create'
 	delete '/logout',   to: 'sessions#destroy'
-	resources :users, only: [:new, :show, :create]
+	resources :users,  only: [:new, :show, :create]
+	resources :events, only: [:new, :create, :show, :index]
 end
