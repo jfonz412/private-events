b0a9cf9a8267ab1a1c0f1ae82b73d386dccd05b3
jfonz412 <jfonzone@outlook.com>
2017-06-13 14:27:00 -0400

    event model and creator association

---
+++ app/models/event.rb
@@ -0,0 +1,4 @@
+class Event < ApplicationRecord
+	# refers to the user that created this event as "creator"
+	belongs_to :creator, :class_name => "User" 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+	# because events refers to the user that created in as "creator", 
+	# we specify the foreign key that will refer to the user(event creator) id
+	has_many :events, :foreign_key => "creator_id"
 	validates :name, presence: true
 end
