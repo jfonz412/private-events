13ce3496a9792fa8cf2c9284fcb8d414b47bdf7e
jfonz412 <jfonzone@outlook.com>
2017-06-13 15:27:03 -0400

    shared errors and event title

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,25 @@ class EventsController < ApplicationController
   end
 
   def create
+  	@event = current_user.events.build(event_params)
+  	if @event.save
+  		flash[:success] = "Event created!"
+  		redirect_to @event
+  	else
+  		flash[:danger] = "There was an error in your event posting..."
+  		render 'new'
+  	end
   end
 
   def show
+  	@event = Event.find(params[:id])
   end
 
   def index
   end
+
+  private
+  	def event_params
+  		params.require(:event).permit(:name, :description)
+  	end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
 	# refers to the user that created this event as "creator"
 	belongs_to :creator, :class_name => "User" 
+	validates :name, presence: true
 end
